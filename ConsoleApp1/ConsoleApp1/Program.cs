96da785b0a50e106831c113cfbd82c80b4c0fd7e
Jan Musiał <s28977@pjwstk.edu.pl>
2024-03-08 17:02:01 +0100

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+//short
+int number = 5;
+//double, float, decimal (decimal is most precise float)  
+var variable = 1;
+var url = string.Empty; 
+var variableName = DoSomething(); // its better to specify type, your collegues will be able to read it
+int[] arrayNumbers = { 1, 2, 3, 4 };
+double[] doubleNumbers = { 0.1, 0.2, 0.3 };
+
+//Functions
+void Foo(int n1)
+{
+    //perform some operations
+}
+
+int ExampleNumber()
+{
+    return 1;
+}
